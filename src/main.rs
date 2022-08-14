93c38a83b3fc4d0d4faf8a026e1e903cae6e8617
Rooted <modhanami@gmail.com>
2022-08-14 13:51:50 +0700

    Refactor for use with data repository

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use std::{env, fs};
 use std::collections::HashMap;
-use std::env;
 use std::error::Error;
 
-use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, Responder, web};
-use actix_web::dev::{Response, ResponseHead};
 use chrono::prelude::*;
+use octocrab::Octocrab;
 use rspotify::model::{FullTrack, PlayHistory, TrackId};
 use serde::{Deserialize, Serialize};
+use tokio::time::Instant;
 
 #[derive(Deserialize)]
 struct History {
@@ -20,73 +20,78 @@ struct TrackPlayCount {
 }
 
 #[derive(Serialize)]
-struct ApiResponse<T> {
+struct FileContent<T> {
     data: T,
     last_updated: DateTime<Utc>,
 }
 
-#[derive(Deserialize)]
-struct PlayCountQuery {
-    t: Option<String>,
-}
-
 struct GitHubInfo {
-    user: String,
     pat: String,
-    repo: String,
+    gist_id: String,
     data_path: String,
 }
 
-#[actix_web::main]
-async fn main() {
-    let addr = ("localhost", 38080);
-    println!("Listening on {}:{}", addr.0, addr.1);
-
-    HttpServer::new(|| {
-        App::new()
-            .service(play_count)
-    })
-        .bind(addr).unwrap()
-        .run()
-        .await.expect("TODO: panic message");
-}
-
-#[get("/play-count")]
-async fn play_count(query: web::Query<PlayCountQuery>) -> impl Responder {
-    match query.t.as_ref().unwrap_or(&"".to_string()).as_str() {
-        "last-30-days" => (),
-        _ => return HttpResponse::BadRequest().body("Invalid query"),
-    }
-
-    let gh_info = match get_github_info() {
-        Ok(info) => info,
-        Err(e) => {
-            println!("{}", e);
-            return HttpResponse::InternalServerError().body("Failed to get GitHub info");
-        }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
+    let days = match env::var("DAYS") {
+        Ok(days) => days.parse::<usize>()?,
+        Err(_) => 1,
     };
-    let history = get_last_x_days_play_history(3, false, gh_info).await.unwrap();
+    let gh_info = get_github_info()?;
 
+    let history = get_last_x_days_play_history(days, &gh_info.data_path, false).await?;
     let mut summarized_track_play_count = summarize_play_counts(history);
     summarized_track_play_count.sort_by(|a, b| b.count.cmp(&a.count));
 
-    HttpResponse::Ok().json(ApiResponse {
+    let file_content = FileContent {
         data: summarized_track_play_count,
         last_updated: Utc::now(),
-    })
+    };
+
+    let file_content_json_pretty = serde_json::to_string(&file_content)?;
+    let file_name = format!("last-{}-days-play-counts.json", days);
+    let octocrab = create_octocrab(gh_info.pat)?;
+    write_to_gist(&file_name, &file_content_json_pretty, &gh_info.gist_id, &octocrab).await;
+
+    let elapsed = start.elapsed();
+    println!("took {:?}", elapsed);
+
+    Ok(())
+}
+
+fn create_octocrab(pat_token: String) -> Result<Octocrab, Box<dyn Error>> {
+    match octocrab::OctocrabBuilder::new().personal_token(pat_token).build() {
+        Ok(octocrab) => Ok(octocrab),
+        Err(e) => Err(e.into()),
+    }
+}
+
+async fn write_to_gist(file_name: &str, file_content: &str, gist_id: &str, octocrab: &Octocrab) {
+    match octocrab.gists()
+        .update(gist_id)
+        .file(file_name)
+        .with_content(file_content)
+        .send()
+        .await {
+        Ok(_) => {
+            println!("successfully updated gist");
+        }
+        Err(e) => {
+            eprintln!("failed to update gist: {:?}", e);
+        }
+    }
 }
 
 fn get_github_info() -> Result<GitHubInfo, Box<dyn Error>> {
-    let gh_user = env::var("GH_USER")?;
-    let gh_pat = env::var("GH_PAT")?;
-    let gh_repo = env::var("GH_REPO")?;
-    let gh_data_path = env::var("GH_DATA_PATH").unwrap_or("data".to_string());
+    let data_path = env::var("GH_DATA_PATH").unwrap_or("data".to_string());
+    let pat = env::var("GH_PAT")?;
+    let gist_id = env::var("GH_GIST_ID")?;
 
     Ok(GitHubInfo {
-        user: gh_user,
-        pat: gh_pat,
-        repo: gh_repo,
-        data_path: gh_data_path,
+        pat,
+        gist_id,
+        data_path,
     })
 }
 
@@ -110,38 +115,38 @@ fn summarize_play_counts(history: Vec<PlayHistory>) -> Vec<TrackPlayCount> {
     track_play_counts
 }
 
-async fn get_last_x_days_play_history(x: usize, latest_first: bool, gh_info: GitHubInfo) -> Result<Vec<PlayHistory>, Box<dyn Error>> {
+async fn get_last_x_days_play_history(days: usize, data_path: &str, latest_first: bool) -> Result<Vec<PlayHistory>, Box<dyn Error>> {
     let mut play_history = Vec::new();
     let mut date = Utc::now().date();
 
-    for _ in 0..x {
-        let mut history = get_play_history_by_date(&date, &gh_info).await?;
+    for _ in 0..days {
+        match get_play_history_by_date(&date, data_path).await {
+            Ok(mut history) => {
+                println!("got {} play history items for {}", history.items.len(), date);
 
-        if latest_first {
-            // the items played_at is in ascending order, reverse it to get the last played item first
-            history.items.reverse();
-        }
+                if latest_first {
+                    // the items played_at is in ascending order, reverse it to get the last played item first
+                    history.items.reverse();
+                }
 
-        play_history.extend(history.items);
+                play_history.extend(history.items);
+            }
+            Err(e) => {
+                eprintln!("failed to get play history for date {}: {:?}", date, e);
+            }
+        };
+
         date = date.pred();
     }
 
     Ok(play_history)
 }
 
-async fn get_play_history_by_date(date: &Date<Utc>, gh_info: &GitHubInfo) -> Result<History, Box<dyn Error>> {
+async fn get_play_history_by_date(date: &Date<Utc>, data_path: &str) -> Result<History, Box<dyn Error>> {
     let filename = format!("{}-{:02}-{:02}.json", date.year(), date.month(), date.day());
-    println!("getting play history on {}", filename);
-
-    let octocrab = octocrab::OctocrabBuilder::new().personal_token(gh_info.pat.clone()).build()?;
-
-    let content_path = format!("{}/{}", gh_info.data_path, filename);
-    let content_items = octocrab.repos(gh_info.user.clone(), gh_info.repo.clone()).get_content().path(content_path).send().await?;
-    let content = match content_items.items[0].decoded_content() {
-        Some(c) => c,
-        None => return Err("No content".into()),
-    };
-    let history = serde_json::from_str::<History>(&content)?;
+    let path = format!("{}/{}", data_path, filename);
+    let file_content = fs::read_to_string(&path)?;
+    let history: History = serde_json::from_str(&file_content)?;
 
     Ok(history)
 }
